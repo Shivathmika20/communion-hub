ec3437417fb202e37c7eb965b66c85a3d920bb62
Shivathmika <130683081+Shivathmika20@users.noreply.github.com>
2025-03-15 17:37:48 +0530

    Update events.query.ts

--- actions/events.query.ts
+++ actions/events.query.ts
@@ -1,21 +1,23 @@
 'use server'
 import {prisma} from "@/lib/prisma";
 
-export async function getEvents(){
-    try{
-        const events= await prisma.event.findMany(
-            {
-                orderBy:{
-                    date:'asc'
-                },
-                  select: { id: true, title: true, date: true, location: true, description: true, imageUrl: true, category: true },
-                  take: 20,
-            }
-        )
-        return events
-    }
-    catch(e){
-        console.log(e)
-        return []
+export async function getEvents() {
+    try {
+        const events = await prisma.event.findMany({
+            select: {
+                id: true,
+                title: true,
+                date: true,
+                location: true,
+                imageUrl: true,
+                category: true,
+            },
+            orderBy: { date: "asc" },
+            take: 20, // Reduce number of results
+        });
+        return events;
+    } catch (error) {
+        console.error("Error fetching events:", error);
+        return [];
     }
 }
